413077e6b8ff753d5597c21d50f98293a11b6bce
Brett Kuskie <fullaxx@gmail.com>
2023-09-07 18:42:47 +0000

    Update copyright

--- test_random.c
+++ test_random.c
@@ -1,6 +1,6 @@
 /*
 	A numerically ordered linked-list queue written in ANSI C
-	Copyright (C) 2022 Brett Kuskie <fullaxx@gmail.com>
+	Copyright (C) 2023 Brett Kuskie <fullaxx@gmail.com>
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
